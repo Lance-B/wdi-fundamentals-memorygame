b5dce2252396d2aeaf5bcf3fdfa82fbac7268cd2
lance bock <14bockl@gmail.com>
2016-05-23 08:10:01 +0000

    yo

--- js/main.js
+++ js/main.js
@@ -2,3 +2,21 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+var cards = [cardOne, cardTwo, cardThree, cardFour]
+
+
+if (cardOne === cardTwo) {
+    alert('You found a match!');
+} else if (cardTwo === cardThree) {
+    alert('You found a match!');
+} else if (cardThree === cardFour) {
+    alert('You found a match!');
+} else {alert('Sorry, try again.') }
+
+var gameBoard = document.getElementById('game-board')
+
+var createBoard = function() {
+    cards = document.createElement('dev');
+    cards.className = 'card';
+    document.getElementsByClassName('card').appendChild(gameBoard);
+}
