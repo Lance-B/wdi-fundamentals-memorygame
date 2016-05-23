d1a773e850b8f63b198a1036db2120d5fb43140b
lance bock <14bockl@gmail.com>
2016-05-23 14:02:06 +0000

    final

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-var cards = [cardOne, cardTwo, cardThree, cardFour]
-
+var cardList = [cardOne, cardTwo, cardThree, cardFour];
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
 if (cardOne === cardTwo) {
     alert('You found a match!');
@@ -11,12 +12,25 @@ if (cardOne === cardTwo) {
     alert('You found a match!');
 } else if (cardThree === cardFour) {
     alert('You found a match!');
-} else {alert('Sorry, try again.') }
+} else {alert('Sorry, try again.'); }
 
-var gameBoard = document.getElementById('game-board')
+var gameBoard = document.getElementById('game-board');
 
 var createBoard = function() {
-    cards = document.createElement('dev');
-    cards.className = 'card';
+    cardList = document.createElement('div');
+    cardList.className = 'card';
     document.getElementsByClassName('card').appendChild(gameBoard);
+}
+
+for (var i = 0; i < cards.length; i++) {
+    cards.setAttribute('data-card', cards[i]);
+    cards.addEventListener('click', isTwoCards);
+}
+
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
 }
